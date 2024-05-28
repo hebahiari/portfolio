ffa2b932658db57d46b25c6c619015d42433373f
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-28 10:42:56 -0700

    set up main files

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </main>
   );
 }
