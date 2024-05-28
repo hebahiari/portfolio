4667a4d99968b8dc63f43ba777bf2eb1d0aef400
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-28 12:15:30 -0700

    updated hero

--- app/components/Hero/Hero.js
+++ app/components/Hero/Hero.js
@@ -2,7 +2,40 @@ import './Hero.css'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className='hero'>
+            <p className='darkblue'>&#60;div&#62;</p>
+            <div className='tab'>
+                <div>
+                    <span className='darkblue'>&#60;h1</span>
+                    <span className='lightblue'> classname</span>
+                    <span className='white'>=</span>
+                    <span className='orange'>'title'</span>
+                    &#62;
+                    <div className='portfolio'>
+                        <h1>
+                            Portfolio
+                        </h1>
+                    </div>
+                    <div className='darkblue'>&#60;/h1&#62;</div>
+                </div>
+                <div>
+                    <span className='darkblue'>&#60;h1</span>
+                    <span className='lightblue'> classname</span>
+                    <span className='white'>=</span>
+                    <span className='orange'>'developer'</span>
+                    &#62;
+                    <div className='portfolio'>
+                        <h2>
+                            Heba A.
+                        </h2>
+                    </div>
+                    <div className='darkblue'>&#60;/h1&#62;</div>
+                </div>
+
+            </div>
+            <div className='darkblue'>&#60;/div&#62;<span className='text-cursor'>|</span></div>
+
+        </div>
     )
 }
 
--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className='nav'>
             <Link href='/' className='title'>Heba Al.</Link>
-            <div className='links'>
+            <div className='links yellow'>
                 <Link href='#projects'>projects</Link>
                 <Link href='#about'>about</Link>
                 <Link href='#contact'>contact</Link>
