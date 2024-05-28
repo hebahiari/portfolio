f3a51b3fc2fb9652b836ec05fd4ea8f738c52ae4
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-28 11:48:19 -0700

    updated  navbar

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,8 +1,16 @@
+import Link from 'next/link'
 import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='nav'>
+            <Link href='/' className='title'>Heba Al.</Link>
+            <div className='links'>
+                <Link href='#projects'>projects</Link>
+                <Link href='#about'>about</Link>
+                <Link href='#contact'>contact</Link>
+            </div>
+        </div>
     )
 }
 
