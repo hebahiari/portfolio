8ce893805176ce9843ff4ee6d59a1e3a7d7aae5c
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-28 11:23:52 -0700

    created background effect

---
+++ app/components/Background/Background.js
@@ -0,0 +1,12 @@
+import './Background.css'
+
+const Background = () => {
+    return (
+        <>
+            <div class="gradient one"></div>
+            <div class="gradient two"></div>
+        </>
+    )
+}
+
+export default Background
--- app/page.js
+++ app/page.js
@@ -6,10 +6,12 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import Background from "./components/Background/Background";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Background />
       <Navbar />
       <Hero />
       <About />
