227770c5c04024539568c33249c94168e125f9e3
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-28 19:09:25 -0700

    about section updates

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -40,46 +40,56 @@ import { SiAutodesk } from "react-icons/si";
 
 const About = () => {
     return (
-        <div className='aboutContainer'>
+        <div className='aboutContainer' id='about'>
             <div className='aboutRight'>
 
             </div>
-            <h3>My Stack</h3>
-            <div className='aboutLeft orange'>
-                <div className='aboutRow'>
-                    <div className='aboutTitle darkblue'>Frontend:</div>
-                    <div className='aboutItem orange'><RiNextjsFill /> NextJs</div>
-                    <div className='aboutItem'><FaReact /> React</div>
-                    <div className='aboutItem'><SiTypescript /> TypeScript</div>
-                    <div className='aboutItem'><IoLogoJavascript /> JavaScript</div>
+
+            <div className='aboutStack'>
+                <h3 className='stackTitle yellow'>Main Stack</h3>
+                <div className='aboutLeft orange'>
+                    <div className='aboutRow'>
+                        <div className='aboutTitle darkblue'>Frontend:</div>
+                        <div className='aboutItem orange'><RiNextjsFill /> NextJs</div>
+                        <div className='aboutItem'><FaReact /> React</div>
+                        <div className='aboutItem'><SiTypescript /> TypeScript</div>
+                        <div className='aboutItem'><IoLogoJavascript /> JavaScript</div>
+                    </div>
+                    <div className='aboutLine'></div>
+                    <div className='aboutRow'>
+                        <div className='aboutTitle darkblue'>Backend:</div>
+                        <div className='aboutItem'><FaNode /> Nodejs</div>
+                        <div className='aboutItem'><SiExpress /> Express</div>
+                        <div className='aboutItem'><SiKnexdotjs /> Knex</div>
+                        <div className='aboutItem'><SiPostgresql /> PostgreSQL</div>
+                    </div>
+                    <div className='aboutLine'></div>
+                    <div className='aboutRow'>
+                        <div className='aboutTitle darkblue'>ML:</div>
+                        <div className='aboutItem'><FaPython /> Python</div>
+                        <div className='aboutItem'><SiPytorch /> PyTorch</div>
+                        <div className='aboutItem'><GoGraph /> matplotlib</div>
+                        <div className='aboutItem'><FiBox /> PIL</div>
+                    </div>
+                    <div className='aboutLine'></div>
+                    <div className='aboutRow'>
+                        <div className='aboutTitle darkblue'>Cloud:</div>
+                        <div className='aboutItem'><FaAws /> AWS</div>
+                        <div className='aboutItem'><SiMongodb /> MongoDB</div>
+                        <div className='aboutItem'><GrHeroku /> Heroku</div>
+                        <div className='aboutItem'><IoLogoVercel /> Vercel</div>
+                    </div>
+                    <div className='aboutLine'></div>
+                    <div className='aboutRow'>
+                        <div className='aboutTitle darkblue'>Design:</div>
+                        <div className='aboutItem'><FiFigma /> Figma</div>
+                        <div className='aboutItem'><SiAdobephotoshop />Photoshop</div>
+                        <div className='aboutItem'><SiAdobeindesign />Indesign</div>
+                        <div className='aboutItem'><SiAutodesk /> 3Ds Max</div>
+                    </div>
                 </div>
-                <div className='aboutRow'>
-                    <div className='aboutTitle darkblue'>Backend:</div>
-                    <div className='aboutItem'><FaNode /> Nodejs</div>
-                    <div className='aboutItem'><SiExpress /> Express</div>
-                    <div className='aboutItem'><SiKnexdotjs /> Knex</div>
-                    <div className='aboutItem'><SiPostgresql /> PostgreSQL</div>
-                </div>
-                <div className='aboutRow'>
-                    <div className='aboutTitle darkblue'>Machine Learning:</div>
-                    <div className='aboutItem'><FaPython /> Python</div>
-                    <div className='aboutItem'><SiPytorch /> PyTorch</div>
-                    <div className='aboutItem'><GoGraph /> matplotlib</div>
-                    <div className='aboutItem'><FiBox /> PIL</div>
-                </div>
-                <div className='aboutRow'>
-                    <div className='aboutTitle darkblue'>Cloud:</div>
-                    <div className='aboutItem'><FaAws /> AWS</div>
-                    <div className='aboutItem'><SiMongodb /> MongoDB</div>
-                    <div className='aboutItem'><GrHeroku /> Heroku</div>
-                    <div className='aboutItem'><IoLogoVercel /> Vercel</div>
-                </div>
-                <div className='aboutRow'>
-                    <div className='aboutTitle darkblue'>Design:</div>
-                    <div className='aboutItem'><FiFigma /> Figma</div>
-                    <div className='aboutItem'><SiAdobephotoshop />Photoshop</div>
-                    <div className='aboutItem'><SiAdobeindesign />Indesign</div>
-                    <div className='aboutItem'><SiAutodesk /> 3Ds Max</div>
+                <div className='aboutStackFooter yellow'>
+                    <div>...and more!</div>
                 </div>
             </div>
         </div>
