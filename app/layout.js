534b77453b716a741453f4f77efc5052bad67603
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-06-08 08:08:09 -0700

    updated robots.txt

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="robots" content="noindex" />
         <script defer src="https://app.tinyanalytics.io/pixel/7JCvjangLJk8vxFn"></script>
       </head>
       <body>{children}</body>
